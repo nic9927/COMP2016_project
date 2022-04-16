0e00636dd6692f2eff3e4ace659ad1978ce43a22
nic9927 <103755596+nic9927@users.noreply.github.com>
2022-04-16 17:41:37 +0800

    Update group11_dbinsert.sql

--- src/group11_dbinsert.sql
+++ src/group11_dbinsert.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books(
 
 
 
-(Many to Many Relationships?)
+
 CREATE TABLE Borrow(
 	b_date DATE,
 	d_date DATE,
@@ -27,7 +27,7 @@ CREATE TABLE Borrow(
 	FOREIGN KEY (book) REFERENCES Books(call_no),
 	FOREIGN KEY (borrower) REFERENCES Students(sno));
 
-(One to Many Relationships?)
+
 CREATE TABLE Reserve(
 	sno CHAR(10),
 	book CHAR(20),
